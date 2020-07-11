9e907ec6f453670a6898a31e6664dcd89f6031a1
Martin Pastore <martinpablopastore@gmail.com>
2020-07-11 15:04:11 -0300

    Improve code structure

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,32 +1,27 @@
 import styled, { css } from 'styled-components';
 
+const STATES = {
+    'primary': css`
+        background-color: #90caf9;
+        color: #fff;
+    `,
+    'error': css`
+        background-color: #CC0000;
+        color: #fff;
+    `,
+    'success': css`
+        background-color: #007E33;
+        color: #fff;
+    `
+}
+
 export const StyledButton = styled.button`
     font-size: 1rem;
     border: none;
     font-weight: 500;
     padding: 1rem; 
     border-radius: ${({ rounded }) => rounded ? '4px' : '0'};
-    ${({ state }) => {
-        switch (state) {
-            case 'primary':
-                return css`
-                    background-color: #90caf9;
-                    color: #fff;
-                `;
-            case 'error':
-                return css`
-                    background-color: #CC0000;
-                    color: #fff;
-                `;
-            case 'success':
-                return css`
-                    background-color: #007E33;
-                    color: #fff;
-                `;
-            default:
-                return '#e0e0e0';
-        }
-    }};
+    ${({ state }) => STATES[state]}};
 
     &:hover {
         opacity: .9;
